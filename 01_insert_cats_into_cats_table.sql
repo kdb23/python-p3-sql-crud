b591e91233f179d466325a272314ecd65a38a8c0
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-25 20:55:27 -0400

    working with SQL

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+INSERT INTO cats (name, age, breed) VALUES ('Pookie', 15, 'Unknown');
+INSERT INTO cats (name, age, breed) VALUES (NULL, NULL, "Tabby");
